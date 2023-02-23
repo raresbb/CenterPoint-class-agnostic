6852260f8f6755d8609889c28f10f6043df2bcf2
raresbb <66315973+raresbb@users.noreply.github.com>
2023-02-23 18:26:06 +0100

    added resume parser in train.py

--- tools/train.py
+++ tools/train.py
@@ -53,10 +53,15 @@ def parse_args():
         action="store_true",
         help="automatically scale lr with the number of gpus",
     )
+    parser.add_argument(
+        "--resume-from",
+        default=None,
+        help="the checkpoint file to resume from")
+    
     args = parser.parse_args()
     if "LOCAL_RANK" not in os.environ:
         os.environ["LOCAL_RANK"] = str(args.local_rank)
-
+        
     return args
 
 
