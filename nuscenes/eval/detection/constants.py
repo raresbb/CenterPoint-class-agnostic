d06206f02a62f08752b916b78527ce31e38c1375
ext-baroianu <rares-cristian.baroianu@stud.tu-darmstadt.de>
2023-06-06 22:31:37 +0200

    yes

---
+++ nuscenes/eval/detection/constants.py
@@ -0,0 +1,35 @@
+# nuScenes dev-kit.
+# Code written by Oscar Beijbom and Varun Bankiti, 2019.
+
+# START MODIFIED RARES
+# Variable definiton for class-agnostic CenterPoint
+DETECTION_NAMES = ['object']
+
+PRETTY_DETECTION_NAMES = {'object': 'Object'}
+
+DETECTION_COLORS = {'object': 'C0'}
+# END MODIFIED RARES
+
+
+ATTRIBUTE_NAMES = ['pedestrian.moving', 'pedestrian.sitting_lying_down', 'pedestrian.standing', 'cycle.with_rider',
+                   'cycle.without_rider', 'vehicle.moving', 'vehicle.parked', 'vehicle.stopped']
+
+PRETTY_ATTRIBUTE_NAMES = {'pedestrian.moving': 'Ped. Moving',
+                          'pedestrian.sitting_lying_down': 'Ped. Sitting',
+                          'pedestrian.standing': 'Ped. Standing',
+                          'cycle.with_rider': 'Cycle w/ Rider',
+                          'cycle.without_rider': 'Cycle w/o Rider',
+                          'vehicle.moving': 'Veh. Moving',
+                          'vehicle.parked': 'Veh. Parked',
+                          'vehicle.stopped': 'Veh. Stopped'}
+
+TP_METRICS = ['trans_err', 'scale_err', 'orient_err', 'vel_err', 'attr_err']
+
+PRETTY_TP_METRICS = {'trans_err': 'Trans.', 'scale_err': 'Scale', 'orient_err': 'Orient.', 'vel_err': 'Vel.',
+                     'attr_err': 'Attr.'}
+
+TP_METRICS_UNITS = {'trans_err': 'm',
+                    'scale_err': '1-IOU',
+                    'orient_err': 'rad.',
+                    'vel_err': 'm/s',
+                    'attr_err': '1-acc.'}
